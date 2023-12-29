6b9eae9d9e0c4f7f6f28c1fd52789566d3981d5e
Being_Mizan <71205865+mizanur87@users.noreply.github.com>
2023-12-30 02:24:24 +0600

    Create triangle_pattern_Serial_Numeric_solve.cpp

---
+++ triangle_pattern_Serial_Numeric_solve.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n ;
+
+    cout << "Input:";
+    cin >> n;
+    int count=1;
+
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout << count << " ";
+            count = count + 1;
+            j++;
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
