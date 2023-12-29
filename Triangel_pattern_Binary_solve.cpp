a236320e20da30a8092ec88308f69a87c3743db8
Being_Mizan <71205865+mizanur87@users.noreply.github.com>
2023-12-30 04:02:43 +0600

    Create Triangel_pattern_Binary_solve.cpp

---
+++ Triangel_pattern_Binary_solve.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n ;
+
+    cout << "Input:";
+    cin >> n;
+    
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            if (j%2==0){
+                cout << "0" ;
+            }
+            else {
+                cout << "1";
+            }
+        }
+        cout <<endl;
+    }
+    
+    return 0;
+}
